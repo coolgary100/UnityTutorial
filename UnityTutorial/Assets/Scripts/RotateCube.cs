2fca1b3c8ad073c1baee5bf4a1dc33a34932724f
coolgary100 <bowengary@hotmail.com>
2019-03-21 12:25:14 -0700

    rotate cube

--- UnityTutorial/Assets/Scripts/RotateCube.cs
+++ UnityTutorial/Assets/Scripts/RotateCube.cs
@@ -4,24 +4,15 @@ public class RotateCube : MonoBehaviour
 {
     private GameObject cube;
 
-    private static class Constants
-    {
-        public const string CubeGameobjectName = "Cube";
-        public const int RotationSpeed = 50;
-    }
+    public int RotationSpeed = 50;
 
     private void Start()
     {
-        cube = GameObject.Find(Constants.CubeGameobjectName);
+        cube = GameObject.Find("Cube");
     }
 
     private void Update()
     {
-        RotateForOneFrame();
-    }
-
-    private void RotateForOneFrame()
-    {
-        cube.transform.Rotate(new Vector3(0, Time.deltaTime * Constants.RotationSpeed, 0));
+        cube.transform.Rotate(new Vector3(0, Time.deltaTime * RotationSpeed, 0));
     }
 }
